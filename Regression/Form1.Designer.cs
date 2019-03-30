eaa0624af4a4f64e084548d5ba7fd5339bed5e20
Carl1016 <410335011@gms.ndhu.edu.tw>
2019-03-30 15:11:36 +0800

    加入專案檔案。

---
+++ Regression/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Regression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Training";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(52, 66);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(435, 333);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Regression/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Regression
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double AverageX = 0, AverageY = 0;
+        static double Numerator = 0;
+        static double Denominator = 0;
+        static double RCB;
+        static double RCA;
+        static double Residual_SS = 0;
+        static double Regression_SS = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String filePath = "./hw1_data1.csv";
+            DataTable tb = new DataTable();
+            read_csv(filePath, ref tb);
+            double[] X = new double[100];
+            double[] Log_Y = new double[100];
+            double[] Y = new double[100];
+            double[,] dataset = new double[100, 2];
+            Convert2Array(ref X, ref Y, ref Log_Y, tb);
+            draw_origin_point(ref X, ref Y);
+            Build_train_dataset(ref X, ref Log_Y,ref dataset);
+            LinearRegression(dataset,ref X);
+
+        }
+        public void LinearRegression(double[,] point,ref double[] X)
+        {
+            if (point.GetLength(0) < 2)
+            {
+                Console.WriteLine("點的數量小於2，無法進行線性回歸");
+                return;
+            }
+            for (int i = 0; i < point.GetLength(0); i++)
+            {
+                AverageX += point[i, 0];
+                AverageY += point[i, 1];
+            }
+            AverageX /= point.GetLength(0);
+            AverageY /= point.GetLength(0);
+            Console.WriteLine("平均X: {0}\n平均Y: {1}", AverageX, AverageY);
+            for (int i = 0; i < point.GetLength(0); i++)
+            {
+                Numerator += (point[i, 0] - AverageX) * (point[i, 1] - AverageY);
+                Denominator += (point[i, 0] - AverageX) * (point[i, 0] - AverageX);
+            }
+            RCB = Numerator / Denominator;
+            RCA = AverageY - RCB * AverageX;
+            draw_spline(ref X);
+            Console.WriteLine("回歸係數A： " + RCA.ToString("0.0000"));
+            Console.WriteLine("回歸係數B： " + RCB.ToString("0.0000"));
+            Console.WriteLine(string.Format("方程為： y = {0} + {1} * x",
+                RCA.ToString("0.0000"), RCB.ToString("0.0000")));
+            for (int i = 0; i < point.GetLength(0); i++)
+            {
+                Residual_SS += (point[i, 1] - RCA - RCB * point[i, 0]) * (point[i, 1] - RCA - RCB * point[i, 0]);
+                Regression_SS += (RCA + RCB * point[i, 0] - AverageY) * (RCA + RCB * point[i, 0] - AverageY);
+            }
+            label1.Text = "回歸係數A： " + RCA.ToString("0.0000") + "\n" +
+                "回歸係數B： " + RCB.ToString("0.0000") + "\n" + string.Format("方程為： y = {0} + {1} * x",
+                RCA.ToString("0.0000"), RCB.ToString("0.0000")) + "\n" +
+                "剩餘平方和： " + Residual_SS.ToString("0.0000") + "\n" +
+                "回歸平方和： " + Regression_SS.ToString("0.0000")+ "\n";
+
+            Console.WriteLine("剩餘平方和： " + Residual_SS.ToString("0.0000"));
+            Console.WriteLine("回歸平方和： " + Regression_SS.ToString("0.0000"));
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void read_csv(String filePath, ref DataTable dt)
+        {
+            FileStream fs = new FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+            string strLine = "";
+            string[] aryLine;
+            int columnCount = 0;
+            bool IsFirst = true;
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                aryLine = strLine.Split(',');
+                if (IsFirst == true)
+                {
+                    IsFirst = false;
+                    columnCount = aryLine.Length;
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        DataColumn dc = new DataColumn(aryLine[i]);
+                        dt.Columns.Add(dc);
+                    }
+                }
+                else
+                {
+                    DataRow dr = dt.NewRow();
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        dr[j] = aryLine[j];
+
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            sr.Close();
+            fs.Close();
+        }
+        public void Convert2Array(ref double[] X, ref double[] Y,ref double[] Log_Y,DataTable tb)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                X[i] = Convert.ToDouble(tb.Rows[i][0]);
+                Log_Y[i] = Convert.ToDouble(tb.Rows[i][2]);
+                Y[i] = Convert.ToDouble(tb.Rows[i][1]);
+            }
+        }
+        public void Build_train_dataset(ref double[] X, ref double[] Log_Y,ref double[,] dataset)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                dataset[i, 0] = X[i];
+                dataset[i, 1] = Log_Y[i];
+            }
+        }
+        public void draw_origin_point(ref double[] X, ref double[] Y)
+        {
+            this.chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            for (int i = 0; i <= 99; i++)
+            {
+                chart1.Series[0].Points.AddXY(X[i], Y[i]);
+            }
+        }
+        public void draw_spline(ref double[]X)
+        {
+            double y = 0;
+            for(int i=0;i<=99;i++)
+            {
+                y = RCA * X[i] + RCB;
+                this.chart1.Series[1].Points.AddXY(X[i], y);
+            }
+        }
+    }
+}
+
